39c77b45dc9c9f5a62812c52a617423b925e47fd
Nicolás Sort Sauquillo <nicolassortsauquillo@gmail.com>
2024-12-18 21:18:08 +0100

    external packages and pyhton things intalled

--- Assets/Scripts/RollerAgent.cs
+++ Assets/Scripts/RollerAgent.cs
@@ -5,7 +5,7 @@ using Unity.MLAgents.Sensors;
 using Unity.MLAgents.Actuators;
 
 public class RollerAgent : Agent
-{ // You can adjust this value as needed
+{
     Rigidbody rBody;
     void Start () {
         rBody = GetComponent<Rigidbody>();
@@ -28,18 +28,8 @@ public class RollerAgent : Agent
                                            Random.value * 8 - 4);
     }
 
-    public override void CollectObservations(VectorSensor sensor)
-    {
-        // Target and Agent positions
-        sensor.AddObservation(Target.localPosition);
-        sensor.AddObservation(this.transform.localPosition);
-
-        // Agent velocity
-        sensor.AddObservation(rBody.velocity.x);
-        sensor.AddObservation(rBody.velocity.z);
-    }
     public float forceMultiplier = 10;
-    public override void OnActionReceived(ActionBuffers actionBuffers)
+public override void OnActionReceived(ActionBuffers actionBuffers)
     {
         // Actions, size = 2
         Vector3 controlSignal = Vector3.zero;
@@ -65,9 +55,9 @@ public class RollerAgent : Agent
     }
 
     public override void Heuristic(in ActionBuffers actionsOut)
-{
-    var continuousActionsOut = actionsOut.ContinuousActions;
-    continuousActionsOut[0] = Input.GetAxis("Horizontal");
-    continuousActionsOut[1] = Input.GetAxis("Vertical");
-}
+    {
+        var continuousActionsOut = actionsOut.ContinuousActions;
+        continuousActionsOut[0] = Input.GetAxis("Horizontal");
+        continuousActionsOut[1] = Input.GetAxis("Vertical");
+    }
 }
